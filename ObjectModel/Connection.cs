5e8b088df8aa3f3b80d7b8a0bd798bbf31714390
Gabrielbemo <gabrielbemoura@gmail.com>
2019-11-24 18:04:55 -0300

    Initial commit

---
+++ ObjectModel/Connection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectModel
+{
+    public class Connection
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Connection()
+        {
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gabri\Desktop\banco\Erripeg.mdf;Integrated Security=True;Connect Timeout=30";
+        }
+
+        public SqlConnection Connect()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        public void Disconnect()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
